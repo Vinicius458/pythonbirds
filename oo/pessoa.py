c777e997e5ee1a53470c4896bfa992ca91abe583
Vinicius458 <viniciuspadua458@hotmail.com>
2020-08-27 19:18:11 -0300

    Sobrescrita de método

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -5,7 +5,7 @@ class Pessoa:
         self.idade= idade
         self.filhos=list(filhos)
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
     @staticmethod
     def metodo_estatico():
        return 42
@@ -14,11 +14,16 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 class Homem(Pessoa):
-    pass
+    def cumprimentar(self):
+        cumprimentar_da_classe=Pessoa.cumprimentar(self) #Ou cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos=3
 
 if __name__ == '__main__':
-    vinicius = Homem(nome='Vinicius')
-    jeferson=Pessoa(vinicius, nome='Jeferson')
+    vinicius = Mutante(nome='Vinicius')
+    jeferson=Homem(vinicius, nome='Jeferson')
     print(Pessoa.cumprimentar(jeferson))
     print(id(jeferson))
     print(jeferson.cumprimentar())
@@ -46,6 +51,10 @@ for filho in jeferson.filhos:
     print(isinstance(pessoa, Homem))
     print(isinstance(vinicius, Pessoa))
     print(isinstance(vinicius, Homem))
+    print(vinicius.olhos)
+    print(jeferson.cumprimentar())
+    print(vinicius.cumprimentar())
+
 
 
 
